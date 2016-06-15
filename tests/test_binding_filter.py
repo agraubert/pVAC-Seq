8f2653a9f9cd40d818a586b9fc3e49b0bba71928
agraubert <captianjroot@live.com>
2016-06-15 09:05:54 -0500

    Added randomized test case to binding_filter

--- tests/test_binding_filter.py
+++ tests/test_binding_filter.py
@@ -5,6 +5,8 @@ from filecmp import cmp
 from subprocess import call
 import sys
 import py_compile
+import random
+import csv
 
 #python -m unittest tests/test_binding_filter.py
 class BindingFilterTests(unittest.TestCase):
@@ -34,3 +36,43 @@ class BindingFilterTests(unittest.TestCase):
             os.path.join(self.test_data_path, "Test_filtered.tsv"),
             False
         ))
+
+    def test_binding_filter_under_random_constraints(self):
+        random.seed()
+        for i in range(5):
+            binding_threshold = random.randint(1,499)
+            fold_change = random.randint(0,100)
+            reader = open(os.path.join(self.test_data_path, 'Test_filtered.xls'), mode='r')
+            csv_reader = csv.DictReader(reader, delimiter='\t')
+            temp_filtered = tempfile.NamedTemporaryFile()
+            writer = open(temp_filtered.name, mode='w')
+            csv_writer = csv.DictWriter(
+                writer,
+                csv_reader.fieldnames,
+                delimiter='\t',
+                lineterminator='\n'
+            )
+            csv_writer.writeheader()
+            for line in csv_reader:
+                if (int(line['MTScore']) < binding_threshold and
+                        float(line['FoldChange']) > fold_change):
+                    csv_writer.writerow(line)
+            reader.close()
+            writer.close()
+
+            temp_out = tempfile.NamedTemporaryFile()
+            binding_filter_cmd = "%s  %s  %s  %s  %s -b %d -c %d" % (
+                sys.executable,
+                self.binding_filter_path,
+                os.path.join(self.test_data_path, "annotated_variants.tsv"),
+                self.fof.name,
+                temp_out.name,
+                binding_threshold,
+                fold_change
+            )
+            self.assertFalse(call([binding_filter_cmd], shell=True))
+            self.assertTrue(cmp(
+                temp_out.name,
+                temp_filtered.name,
+                False
+            ))
